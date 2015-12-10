d9229d40d442c5dc2673de8137dad91c3e3682c5
Scott Christensen <scottmc526@gmail.com>
2015-12-09 22:16:09 -0700

    User can see all five images when looking at playlist page

--- public/js/logic.js
+++ public/js/logic.js
@@ -10,12 +10,25 @@ $(document).ready(function (){
     for (var i = 0; i < 3; i++) {
     var rand = Math.floor(Math.random() * payload['results'].length)
     arr.push(payload['results'][rand]['cover_art'])[i];
-      }
-      return arr;
-      }
+    }
+  return arr;
+  }
 
-      for (var i = 0; i < randomImages(getter).length; i++) {
-        $('.pic').append('<img class="red"src="images/'+randomImages(getter)[0]+'"/>')
-      }
+
+  for (var i = 0; i < randomImages(getter).length; i++) {
+    $('.pic').append('<img class="red"src="images/'+randomImages(getter)[0]+'"/>')
+  }
+
+  var allImages = function () {
+    var arr = [];
+    for (var i = 0; i < payload['results'].length; i++) {
+      arr.push(payload['results'][i]['cover_art'])[i];
+    }
+    return arr;
+  }
+
+  for (var i = 0; i < allImages(getter).length; i++) {
+    $('.scroll').append('<img class="red" src="images/'+allImages(getter)[i]+'"/>')
+  }
   })
 })

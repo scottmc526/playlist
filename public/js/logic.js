7e496034bf5ba728998292a64f54e1a80780ccf0
Scott Christensen <scottmc526@gmail.com>
2015-12-09 19:07:22 -0700

    console logged 3 random album covers in an array

--- public/js/logic.js
+++ public/js/logic.js
@@ -1,9 +1,15 @@
 $(document).ready(function (){
   var getter = $.ajax ({
-    url: 'https://lit-fortress-6467.herokuapp.com/post',
-    method: "POST",
+    url: 'https://lit-fortress-6467.herokuapp.com/object',
+    method: "GET",
+    dataType:'json'
   })
   getter.done(function(payload){
-    console.log(payload);
+    var arr = []
+    for (var i = 0; i < 3; i++) {
+    var rand = Math.floor(Math.random() * payload['results'].length)
+    arr.push(payload['results'][rand]['cover_art'])[i];
+    }
+    console.log(arr);
   })
 })
